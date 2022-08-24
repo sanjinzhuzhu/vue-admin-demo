dcdbd44810c043ed756e4a5bf7932a09281857fb
sanjinzhuzhu <sanjinzhuzhu@aliyun.com>
2022-08-24 18:09:42 +0800

    admin1.1

--- app/src/App.vue
+++ app/src/App.vue
@@ -3,7 +3,7 @@
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <Header></Header>
-    <Footer>我是底部</Footer>
+    <Footer></Footer>
     
   </div>
 </template>
